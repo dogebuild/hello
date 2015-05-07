68257e4aac0f9ae9bb0872f6024c39258dd3f58b
kirillsulim <kirillsulim@gmail.com>
2015-05-08 00:14:26 +0500

    Add github link

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# hello
-Hello world plugin 
+dogebuild-hello
+====
+
+Hello world plugin.
+You can use this as an example to create your own plugin
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name='dogebuild-hello',
       version='0.1',
-      description='',
-      author='',
-      author_email='',
-      url='',
+      description='A hello world plugin for dogebuild.',
+      author='Kirill Sulim',
+      author_email='kirillsulim@gmail.com',
+      url='https://github.com/dogebuild/hello',
       packages=find_packages(include=[
           'dogebuild*',
           ]),
